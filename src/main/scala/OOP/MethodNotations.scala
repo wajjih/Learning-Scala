2d1544647cc03dd15a379260682c7d1ed4c30aee
wajjih <wajjih22@gmail.com>
2023-11-13 11:22:59 -0500

    Done

--- src/main/scala/OOP/MethodNotations.scala
+++ src/main/scala/OOP/MethodNotations.scala
@@ -12,7 +12,11 @@ object MethodNotations extends App {
     def unary_! : String = s"${name}, what good?!"
     def unary_+ : Int = this.age + 1
     def isAlive: Boolean = true
+    def learns(thing: String): String = s"$name learns $thing"
+    def learnsScala: String = this learns "Scala"
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+
+    def apply(watch: Int): String = s"${name} watched ${favoriteMovie} ${watch} times"
 
     override def toString = s"Person($name, $favoriteMovie)"
   }
@@ -39,4 +43,24 @@ object MethodNotations extends App {
 
   println(mary + "Im that nigga")
   println(+mary)
+
+  println(mary learnsScala)
+  println(mary(6))
+
+  /*
+  1. Overload the + operator
+     mary + "the rockstar" => new person "Mary (the rockstar)"
+
+  2. Add an age to the Person Class
+     add a unary + operator => new person with the age +1
+     +mary => mary with the age incrementer
+
+  3. Add a "learns" method in the person Class => "Mary learns Scala"
+     Add a learnsScala method, calls lerns method with "Scala"
+     Use it in a postfix notion
+
+  4. Overload the apply method
+     mary.apply(2) => "Mary watched inception 2 times"
+
+  */
 }
